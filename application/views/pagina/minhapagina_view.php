94b89d7b278e9231aa07ae81e29bf055103e1092
Lucas Figueiredo <lucas-fj@hotmail.com>
2015-04-22 17:23:00 -0300

    1. Imagem Teste de Produto 2. Exclusão de css, por hora, desnecessário. 3. Início de edições da Página.

--- application/views/pagina/minhapagina_view.php
+++ application/views/pagina/minhapagina_view.php
@@ -7,47 +7,84 @@ document.getElementById("cabecalho").innerHTML = "TO DO NOME_PAGINA";
 <!-- Conteúdo da View abaixo -->
 
 <!--CARD SERVICO-->
+<div class="conteudo">
 <div class="card-panel deep-purple lighten-2 z-depth-1">
     <div class="row cardConteudo valign-wrapper">
-            <div class="col s3 right-align cardImagem">
-            <img src="<?php echo base_url('src/imagens/pagina/perfil/harry-square.png'); ?>" class="circle imgServico z-depth-1"/>
-            </div>
-            <div class="col s9 center-align cardInfo">
-                          <div class="row  white-text left-align cardTopo">
-                          <span class="nomeServico">Harry Potter</span><br />
-                          <h6 class="sloganServico">Harry Potter e o Prisioneiro de Azkaban</h6>
-                          <h6 class="enderecoServico">O Armário Sob a Escada Rua dos Alfineiros 4 </h6>
-                          </div>
-                          <div class="row  cardRodape valign-wrapper">
-                          <div class="col s4 left-align  valign-wrapper cardRate">
-                          <i class="mdi-action-star-rate white-text rateServico"></i>
-                          <i class="mdi-action-star-rate white-text rateServico"></i>
-                          <i class="mdi-action-star-rate white-text rateServico"></i>
-                          <i class="mdi-action-star-rate white-text rateServico"></i>
-                          <i class="mdi-action-star-rate white-text rateServico"></i>
-                          </div>
-                          <div class="col s8 right-align  cardBotoes">
-                          <a class="btn-floating waves-effect waves-light deep-purple btnServico"><i class="mdi-action-info-outline"></i></a>
-                          <a class="btn-floating waves-effect waves-light deep-purple btnServico"><i class="mdi-maps-place iconeBotao"></i></a>
-                          <a class="btn-floating waves-effect waves-light deep-purple btnServico"><i class="mdi-communication-comment iconeBotao"></i></a>
-                          <a class="btn-floating waves-effect waves-light deep-purple btnServico"><i class="mdi-social-share valign-wrapper iconeBotao"></i></a>
-                          </div>
-                          </div>
-            </div>
+    <div class="col s3 right-align cardImagem">
+    <img src="<?php echo base_url('src/imagens/pagina/perfil/harry-square.png'); ?>" class="circle imgServico z-depth-1"/>
+    </div>
+    <div class="col s9 center-align cardInfo">
+        <div class="row  white-text left-align cardTopo">
+            <span class="nomeServico">Harry Potter</span><br />
+            <h6 class="sloganServico">Harry Potter e o Prisioneiro de Azkaban</h6>
+            <h6 class="enderecoServico">O Armário Sob a Escada Rua dos Alfineiros 4 </h6>
+        </div>
+        <div class="row  cardRodape valign-wrapper">
+        <div class="col s4 left-align  valign-wrapper cardRate">
+            <i class="mdi-action-star-rate white-text rateServico"></i>
+            <i class="mdi-action-star-rate white-text rateServico"></i>
+            <i class="mdi-action-star-rate white-text rateServico"></i>
+            <i class="mdi-action-star-rate white-text rateServico"></i>
+            <i class="mdi-action-star-rate white-text rateServico"></i>
+        </div>
+        <div class="col s8 right-align  cardBotoes">
+            <a class="btn-floating waves-effect waves-light deep-purple btnServico"><i class="mdi-action-info-outline"></i></a>
+            <a class="btn-floating waves-effect waves-light deep-purple btnServico"><i class="mdi-maps-place iconeBotao"></i></a>
+            <a class="btn-floating waves-effect waves-light deep-purple btnServico"><i class="mdi-communication-comment iconeBotao"></i></a>
+            <a class="btn-floating waves-effect waves-light deep-purple btnServico"><i class="mdi-social-share valign-wrapper iconeBotao"></i></a>
+        </div>
+        </div>
+    </div>
     </div>
 </div>
 
+<div class="contProdutos">
 <!-- PRODUTO -->
-<div class="cardConteudo">
 <div class="row">
-<div class="col s6 m7">
-  <div class="card">
-    <div class="card-image">
-      <img src="<?php echo base_url('src/imagens/pagina/produto/harry-kit.jpg'); ?>">
+    <div class="col s6 m4 l4">
+    <div class="card">
+    <div class="card-image waves-effect waves-block waves-light">
+        <img class="activator" src="<?php echo base_url('src/imagens/pagina/produto/harry-kit.jpg'); ?>">
     </div>
     <div class="card-content">
-    <span class="card-title activator grey-text text-darken-4">Kit de Poções (Felix Felicius)</span>
+        <span class="card-title activator grey-text text-darken-4">Card Title <i class="mdi-navigation-more-vert right"></i></span>
     </div>
-  </div>
+    <div class="card-reveal">
+        <span class="card-title grey-text text-darken-4">Kit de Poções<i class="mdi-navigation-close right"></i></span>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat.</p>
+    </div>
+    </div>
+    </div>
+<!-- PRODUTO -->
+    <div class="col s6 m4 l4">
+    <div class="card">
+    <div class="card-image waves-effect waves-block waves-light">
+        <img class="activator" src="<?php echo base_url('src/imagens/pagina/produto/harry-kit.jpg'); ?>">
+    </div>
+    <div class="card-content">
+        <span class="card-title activator grey-text text-darken-4">Card Title <i class="mdi-navigation-more-vert right"></i></span>
+    </div>
+    <div class="card-reveal">
+        <span class="card-title grey-text text-darken-4">Kit de Poções<i class="mdi-navigation-close right"></i></span>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat.</p>
+    </div>
+    </div>
+    </div>
+<!-- PRODUTO -->
+    <div class="col s12 m4 l4">
+    <div class="card">
+    <div class="card-image waves-effect waves-block waves-light">
+        <img class="activator" src="<?php echo base_url('src/imagens/pagina/produto/harry-kit.jpg'); ?>">
+    </div>
+    <div class="card-content">
+        <span class="card-title activator grey-text text-darken-4">Card Title <i class="mdi-navigation-more-vert right"></i></span>
+    </div>
+    <div class="card-reveal">
+        <span class="card-title grey-text text-darken-4">Kit de Poções<i class="mdi-navigation-close right"></i></span>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat.</p>
+    </div>
+    </div>
+    </div>
+</div>
 </div>
 </div>
