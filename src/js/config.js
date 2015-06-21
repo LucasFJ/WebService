c112f98efc4e059386fca45d36bc85eacf36ccf3
Gabriel <gabriel.cientista123@gmail.com>
2015-06-21 00:53:16 -0300

    1 - Arrumado arquivo config.js e iniciado a implementação de classe de codificação de URL universal 2 - Arrumado quebra de linha na descrição da página 3 - Numero de endereço agora é obrigatório 4 - footer - arrumado inserção dinamica de JS pois faltava o src/js no base_url() 5 - Pagina model - arrumado a alteração de endereço e agora também puxa o cd_hexadecimal para o array de dados

--- src/js/config.js
+++ src/js/config.js
@@ -1,30 +1,5 @@
 base_url = "http://localhost/WebService/";
 
-window.onload = function(){
-//Funções básicas partilhadas por todos os arquivos ou quase.
-// Initialize collapse button
-$('.button-collapse').sideNav();
- 
-//Controla o abre/fecha dos itens da SideNav
-$('.liMenu').click(function(){
-if($(this).hasClass('active')){
-    $(this).children('.collapsible-body').hide(400);
-    $(this).removeClass('active'); 
-} else {
-    $(this).removeClass('active');
-    $('.active').children('.collapsible-body').hide(400);
-    $(this).addClass('active');
-    $('.active').removeClass('active');
-}
-});
-
-//Máscaras de formulários com jQuery Mask
-$('.telefone').mask('(00) 0000-0000');
-$('.celular').mask('(00) 00000-0000');
-$('#cep').mask('00000-000');
-
-$('.select').material_select();
-}
 
 function CriaRequest() {
     try{
@@ -47,3 +22,35 @@ if(!request){
         return request;
     }
 } 
+
+//Funções básicas partilhadas por todos os arquivos ou quase.
+// Initialize collapse button
+$('.button-collapse').sideNav();
+ 
+//Controla o abre/fecha dos itens da SideNav
+$('.liMenu').click(function(){
+if($(this).hasClass('active')){
+    $(this).children('.collapsible-body').hide(400);
+    $(this).removeClass('active'); 
+} else {
+    $(this).removeClass('active');
+    $('.active').children('.collapsible-body').hide(400);
+    $(this).addClass('active');
+    $('.active').removeClass('active');
+}
+});
+
+
+//Máscaras de formulários com jQuery Mask
+$('.telefone').mask('(00) 0000-0000');
+$('.celular').mask('(00) 00000-0000');
+$('#cep').mask('00000-000');
+
+function codificarUrl(str){
+    str = str.replace(/!/g, "%21");
+    str = str.replace(/'/g, "%27");
+    str = str.replace(/\./g,"%2E");
+    str = str.replace(/\(/g,"%28");
+    str = str.replace(/\)/g,"%29");
+    return str;
+}
